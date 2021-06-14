0f6fa15db75a863608a7ce5d835a5ba79cc77ea2
Chris Wood <chriswood.ca@gmail.com>
2021-06-14 16:51:13 -0700

    fix whitespace

--- 40-gen-selfsigned-cert.sh
+++ 40-gen-selfsigned-cert.sh
@@ -1,4 +1,4 @@
 if test "${NGINX_SCHEME#*ssl}" != "$NGINX_SCHEME"; then
-openssl req -x509 -nodes -days 3650 -subj "/C=CA/ST=BC/O=BCIT/CN=bcit.ca" -newkey rsa:2048 -keyout /certs/nginx-selfsigned.key -out /certs/nginx-selfsigned.crt
-echo "SSL enabled"
+  openssl req -x509 -nodes -days 3650 -subj "/C=CA/ST=BC/O=BCIT/CN=bcit.ca" -newkey rsa:2048 -keyout /certs/nginx-selfsigned.key -out /certs/nginx-selfsigned.crt
+  echo "SSL enabled"
 fi
