e1798e1aff62696922f5bd3bd15c5328d1d9805a
Chris Wood <chris_wood@bcit.ca>
2024-09-13 15:58:08 -0700

    Use new image tag, also dockerize enabled sites

--- 50-dockerize-config.sh
+++ 50-dockerize-config.sh
@@ -9,3 +9,11 @@ for tmpl_file in $( find /etc/nginx/conf.d/ -type f -name '*.tmpl' -not -path '*
     dockerize -template "$tmpl_file":"$config_file" \
     && rm -f "$tmpl_file"
 done
+
+for tmpl_file in $( find /etc/nginx/http.d/ -type f -name '*.tmpl' -not -path '*/\.git/*' 2>/dev/null ); do
+    config_file="$( dirname -- "$tmpl_file" )/$( basename -- "$tmpl_file" .tmpl )"
+    echo "dockerizing: ${tmpl_file}"
+    echo "         =>  ${config_file}"
+    dockerize -template "$tmpl_file":"$config_file" \
+    && rm -f "$tmpl_file"
+done
